245714dc537a1d1edc5cf65cd211fe4b70d247ae
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2024-07-21 11:29:32 -0600

    Add some documentation to the syntax elements

--- lib/syntax_printer.dart
+++ lib/syntax_printer.dart
@@ -6,39 +6,97 @@ import 'package:meta/meta.dart';
 import './syntax_definition.dart';
 
 
-// TODO: document all classes here; include a code block showing a "template"
-//  that illustrates the kind of pattern the class is emulating
-
+/// Converts a [SyntaxElement] into its JSON string representation.
+/// 
+/// Each [SyntaxElement] represents a particular kind of TextMate JSON object.
+/// This class simply provides methods for converting these objects into strings.
+/// [print] is primarily responsible for this conversion, and [printToFile] can be
+/// used to easily write the results to a file.
+/// 
+/// This class follows the singleton pattern, and the methods described above can
+/// be accessed via [instance]. This instance requires no additional setup and
+/// completely encapsulates the responsibilities of its encoder.
 class SyntaxPrinter {
+  /// The indentation used for each "scope" in the resulting JSON.
   static const INDENT = "    ";
+  /// The singleton instance to use to access all printing methods.
   static final instance = SyntaxPrinter._create();
 
+  /// The JSON encoder used to pretty-print a [SyntaxElement] into its JSON form.
   late final JsonEncoder _encoder = 
     JsonEncoder.withIndent(INDENT, (item) => item.toJson());
 
+  /// A private constructor that creates an instance of this class.
+  /// Instances should not be created and instead the singleton [instance] should
+  /// be used.
   SyntaxPrinter._create();
 
-  String print(SyntaxElement syntax) {
-    return _encoder.convert(syntax) + "\n";
+  /// "Prints" a given [element] by converting it to a string containing its
+  /// JSON representation. Each [SyntaxElement] represents a kind of JSON object,
+  /// so passing any type of element is acceptable.
+  String print(SyntaxElement element) {
+    return _encoder.convert(element) + "\n";
   }
 
-  Future<void> printToFile(SyntaxElement syntax, String path) {
+  /// "Prints" a given [element] to a given file [path]. Like [print], this function
+  /// can convert any [SyntaxElement] into its JSON string representation. However,
+  /// instead of returning the string, this function will write the resulting JSON
+  /// into a file, returning a [Future] that can be used to `await` the completion
+  /// of the operation.
+  Future<void> printToFile(SyntaxElement element, String path) {
     var file = File(path);
-    var contents = print(syntax);
+    var contents = print(element);
     return file.writeAsString(contents);
   }
 }
 
+/// Guarantees that an implementor can work properly with the JSON encoding process.
 abstract interface class JsonEncodable {
+  /// Converts `this` and returns a [Map] representing a JSON object.
+  /// This allows for easy integration with dart's [JsonEncoder] class.
   Map toJson();
 }
 
 
+/// The base class for various kinds of TextMate JSON elements. 
+/// 
+/// Each subtype of this is intended to be a data class/map for a separate kind
+/// of TextMate element. (See each class' docstring for an example of what it represents.)
+/// It is the responsibility of these subtypes to implement [toJson] and define
+/// all properties needed to create the corresponding JSON representaton.
+/// Some classes may also define conversion methods to other element types.
+/// 
+/// Subclasses of this *must* have [toJson] implemented, as guaranteed by the
+/// [JsonEncodable] interface. This method is responsible for returning the actual
+/// JSON representation of `this` element.
 sealed class SyntaxElement implements JsonEncodable {
   const SyntaxElement();
 }
 
 
+// TODO: finish documenting from here on (does MainBody also need any additional info?)
+/// Represents an entire TextMate object contained in a language file.
+/// This includes the supported file types, the language scope name, root
+/// patterns, and the named repository patterns.
+/// 
+/// ```
+/// {
+///     "fileTypes": [
+///         "txt",
+///         "dart",
+///         // etc
+///     ],
+///     "scopeName": "language.scope.name",
+///     "patterns": [
+///         // <Pattern>,
+///         // ...
+///     ],
+///     "repository": {
+///       // <RepositoryItem>,
+///       // ...
+///     }
+/// }
+/// ```
 final class MainBody extends SyntaxElement {
   final String? scopePrefix;
   final bool isTextSyntax;
